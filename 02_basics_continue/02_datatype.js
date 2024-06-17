d0bb587e4a010eb75d0ca9ea21abbf8b576495f5
Devkaran Aroa <104886931+Darora03@users.noreply.github.com>
2024-06-17 19:05:28 +0000

    datatype concfig done

--- 02_basics_continue/02_datatype.js
+++ 02_basics_continue/02_datatype.js
@@ -1,1 +1,21 @@
+
+
+"use strict";//strict all code as with new java version
+
+console.log(3+3)
 console.log("Hey Dev!")
+let name ="Dev Arora"
+let age ="18"
+let iscompleted=true
+let state=null;
+//age =>number
+//name=>string
+//bigint
+//boolean =>true/false
+//null =>object
+//undefined
+//
+
+console.log("age::"+age)
+console.log("type of ::"+typeof age);
+console.log(typeof state)
