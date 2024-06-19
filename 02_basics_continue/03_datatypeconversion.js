b9d974094ac4e2e0bef6c9ce0f2a44eacbdb1c4f
Devkaran Aroa <104886931+Darora03@users.noreply.github.com>
2024-06-19 13:33:10 +0000

    datatype cofig done

---
+++ 02_basics_continue/03_datatypeconversion.js
@@ -0,0 +1,29 @@
+
+
+//let score=null
+//console.log(typeof score)
+//console.log(typeof (score))
+
+//let isValueNumber= Number(score);
+//console.log(typeof isValueNumber)
+//console.log(isValueNumber)
+//let isNumberVerified=null;
+//let boolenisNumberVerified= Boolean (isNumberVerified);
+//console.log(boolenisNumberVerified)
+//console.log(typeof boolenisNumberVerified)
+
+//console.log("*********");
+//let someNumber=33
+//let springNumber= String (someNumber)
+//let springnumberVerified= String (isNumberVerified)
+//console.log(springNumber)
+//console.log(typeof springNumber)
+//console.log(springnumberVerified)
+//let value =3
+//let negValue= -value;
+//console.log("value"+value)
+ //console.log("negative value would be::"+negValue)
+
+ let str1="Dev"
+ let str2= "Arora"
+ console.log(str1 +" "+str2);
