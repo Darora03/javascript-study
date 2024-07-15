824fe2b9e3e8a7ddd524fc93adf22e1ccf583d6c
Devkaran Aroa <104886931+Darora03@users.noreply.github.com>
2024-07-15 13:08:10 +0000

    constrol flow  Executed

---
+++ control_flow/one.js
@@ -0,0 +1,29 @@
+const score=200
+if(score>100){
+    const power ="Fly"
+    console.log(`User power : ${power}`)
+}
+//console.log(`User power : ${power}`)--This will gave error  as because the scope of IF is in the if , but we are trying to access it as globally
+const balance=1000
+
+if(balance<500){
+    console.log("balance less than 500")
+}else if(balance<750){
+    console.log("balance less than 750")
+}else if(balance<900){
+    console.log("balance less than 900")
+}else{
+    console.log("balance less than 1200")
+}
+const userLoggedIn= true;
+const debitCard =true;
+const loggedInFromGoogle =true;
+const loggedInFromEmail= true;
+
+if(loggedInFromGoogle || loggedInFromEmail){
+    console.log("User successfully logged in ");
+
+}
+if(userLoggedIn && debitCard  ){
+    console.log("User allowed to buy course ");
+}
