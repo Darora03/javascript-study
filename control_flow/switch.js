78d329f3bbad90eca7144b88804e527e3201d373
Devkaran Aroa <104886931+Darora03@users.noreply.github.com>
2024-07-16 09:53:19 +0000

    control flow config done

---
+++ control_flow/switch.js
@@ -0,0 +1,40 @@
+/* Swich syntax
+switch (key){
+    case value:
+        break;
+
+        default:
+            break;
+}
+           */
+          
+
+const dayNumberOFWeek=4;
+switch (dayNumberOFWeek){
+case 1:
+    console.log("Monday");
+    break;
+case 2:
+    console.log("Tuesday ");
+    break;
+case 3:
+    console.log("Wednesday");
+    break;
+case 4:
+    console.log("Thursday");
+    break;
+case 5:
+    console.log("Friday");
+    break;
+case 6:
+    console.log("Saturday");
+    break;
+
+    default:
+        console.log("Sunday");
+        break;
+
+
+
+}
+        
---
+++ control_flow/truthy.js
@@ -0,0 +1,47 @@
+//falsy value 
+//false,0,-0,BIgInt 0n,"",null,undefined,NaN
+
+//truthy value
+//true,"0",'false'," ",[],{},function(){}
+
+//const userEmail="d@devarora.ai";condition 1=when value is true 
+//const userEmail="";empty string it will gave falsy value
+//const userEmail=[];empty array gives truthy value
+//const userEmail=undefined;
+const userEmail=[];
+userEmail?console.log("Got user Email"):console.log("Don't have an user email");
+/*
+if(userEmail){
+    console.log("Got user Email");
+}else{
+    console.log("Don't have an user email");
+    
+}
+    */
+/*
+//to checked weather given array is empty or not
+if(userEmail.length===0){
+    console.log("Array is empty");
+} 
+
+
+//now how can you check wheather given object is empty or not
+
+
+
+const emptyObject ={}
+if(Object.keys(emptyObject).length===0){
+ console.log("Object is empty");
+}
+*/
+/*
+//Nullish coalescing Operator(??):null undefined
+
+let val1;
+//val1= 5??10 =>5
+//val1= null??10 =>10
+//val1= undefined??10 =>10
+//val1 =undefined??10??20 =>20
+val1 =undefined??20??10
+console.log(val1);
+*/
