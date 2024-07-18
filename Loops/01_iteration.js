c29117d5932b262ffe9bb8587e875bdfbd7622b7
Devkaran Aroa <104886931+Darora03@users.noreply.github.com>
2024-07-18 10:32:37 +0000

    Loop config done

---
+++ Loops/01_iteration.js
@@ -0,0 +1,17 @@
+
+const myArray = ["superman","batman","action kamin"]
+let arrIndex=0;
+while(arrIndex<myArray.length){
+   // console.log(myArray[arrIndex]);
+    arrIndex =arrIndex +1;
+
+}
+let number  =0;
+
+do{
+   
+console.log(number);
+
+number= number +2;
+
+}while(number <=10)
---
+++ Loops/02_higher_function.js
@@ -0,0 +1,13 @@
+const myArr = [1,2,3,4,5]
+for(const arr of myArr){
+   // console.log(arr);
+}
+
+const greeetings ="Hello_Duniya"
+for(const greet of greeetings){
+  
+    console.log(`each char is :${greet}`);
+    if(greet==="_"){
+  break;
+    }
+}
