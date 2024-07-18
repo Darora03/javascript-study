43dc4580808ff6c18b82cf1f21d688575deecb67
Devkaran Aroa <104886931+Darora03@users.noreply.github.com>
2024-07-18 10:56:44 +0000

    map config done

--- Map/01_map.js
+++ Map/01_map.js
@@ -7,23 +7,24 @@ map.set('Fr',"France")
 //console.log(map);
 
 for(const [key,value] of map ){
-    console.log(key,":-",value)
+   // console.log(key,":-",value)
 }
 /*
 for(const key of map ){
     console.log(key);
 }
     */
-   /*
+   
+  // for of doesn't work in case of objects
    //object is not iteratable
 //for of iterate doesn't work in case of objects
 //ror: myObject is not iterable
 const myObject = {
-    'game1':"NFS",
-    'game2':"Spiderman"
+    game1:"NFS",
+    game2:"Spiderman"
 }
 for(const [key,value] of myObject){
     console.log(key,":-",value);
     //ror: myObject is not iterable
 }
-    */
+    
