919a6728bd409c16331aaa2ffb40dc5a67c803a0
Devkaran Aroa <104886931+Darora03@users.noreply.github.com>
2024-07-24 04:09:57 +0000

    30_days_code

---
+++ javascript_30day_code/operators.js
@@ -0,0 +1,39 @@
+/*
+Activity 1
+//Arithmatic Operation
+let num1=5;
+let num2=3;
+function addTwoNumber(num1,num2){
+    return num1+num2;
+}
+function subtractTwoNumber(num1,num2){
+    return num1-num2;
+}
+function multiplyTwoNumber(num1,num2){
+    return num1*num2;
+}
+function divideTwoNumber(num1,num2){
+    return num1/num2;
+}
+function reminderOFTwoNumber(num1,num2){
+    return num1%num2;
+}
+let sum = addTwoNumber(num1,num2);
+let subs = subtractTwoNumber(num1,num2);
+let multiply = multiplyTwoNumber(num1,num2);
+let divide=divideTwoNumber(num1,num2);
+let mod=reminderOFTwoNumber(num1,num2);
+console.log(sum);
+console.log(subs);
+console.log(multiply);
+console.log(divide);
+console.log(mod);
+*/
+
+//activity2
+let num=5;
+num+=1;
+console.log(`+=  operator: ${num}`);
+
+num-=1;
+console.log(`-=  operator: ${num}`);
---
+++ javascript_30day_code/variable.js
@@ -0,0 +1,52 @@
+/*
+//task 1
+var num=1;
+console.log(num);
+
+//task 2
+let str= "Devkaran";
+console.log(str);
+
+//task3
+const isHuman = true;
+console.log(isHuman);
+*/
+//task4:
+/*
+const name ="Devkaran"
+let  age =24
+const isMarried=false
+const aboutMe={
+      about:"I am a coder and a  good cricketer as well. In free time i used to sing, singing is my hobby."
+}//object
+const superheroes=['batman','superman','spiderman','flash']
+const date= null;
+
+console.log(`    my name is ${name} and  my age is ${age}.
+    Am i married: ${isMarried}.
+    And also my abouts are : ${aboutMe.about} 
+    and my favourite super heroes are: ${superheroes}`)
+    
+    console.log(` name : ${typeof name}`);
+    console.log(`age : ${typeof age}`);
+    console.log(`about me : ${typeof aboutMe}`);
+    console.log(`is Married : ${typeof isMarried}`);
+    console.log(`super heroes : ${typeof superheroes}`);
+    console.log(`date : ${typeof date}`);
+    console.log(`function: ${typeof function(){}}`);
+  
+ //task 5
+ let num=10
+ console.log(`first num ${num}`)
+ num=20;
+ console.log(`second num ${num}`)
+ 
+//task6
+const num =10;
+console.log(`first val:${num}`)
+num =20;
+console.log(`second val:${num}`)
+*/
+//task7
+
+
